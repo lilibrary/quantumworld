3cd9ccb82050caa3ac6c3de91ac62f695835788f
Mark <quantumx52@gmail.com>
2016-12-01 14:16:44 +0800

    1201

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header()?>
+
+    <h1><?php bloginfo('name') ?></h1>
+    <span>Hello World!</span>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')/?>">
+    <link rel="stylesheet" href="<?php bloginfo('template_url')/?>">
+    <script src="<?php bloginfo('template_url') ?>/main.js>">
+
+<?php get_footer()?>
