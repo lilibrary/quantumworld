5b6a613748994119be9e359b53baf4f26b70467c
Mark <quantumx52@gmail.com>
2016-12-01 14:51:00 +0800

    change head/link

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
     <h1><?php bloginfo('name') ?></h1>
     <span>Hello World!</span>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')/?>">
-    <link rel="stylesheet" href="<?php bloginfo('template_url')/?>">
-    <script src="<?php bloginfo('template_url') ?>/main.js>">
+    <link rel="stylesheet" href="<?php bloginfo('template_url')?>/style.css">
+    <script src="<?php bloginfo('template_url')?>/main.js">
 
 <?php get_footer()?>
