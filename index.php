77f2512fcbcb743bac5d904e3429c8ff9ad5c501
Mark <quantumx52@gmail.com>
2016-12-02 13:12:07 +0800

    1202

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header()?>
 <div class="nav wfont">
-	<div class="nav_content">
+	<div class="nav_content" id="top">
 		<div class="nav_bar">
 			<div></div>
 			<span>首页</span>
@@ -15,8 +15,31 @@
 		</div>
 	</div>
 </div>
-<div class="banner">
+<div class="banner clearfix">
 		<div class="banner_text wfont">没有最好，只有更好。</div>
 		<div class="banner_carousel"></div>
+		<div class="display">
+			<div class="display_title">
+				我们不可思议的项目
+			</div>
+			<div class="display_picarray">
+				<div class="display_picarray_ur1 display_picarray_ur"></div>
+				<div class="display_picarray_ur2 display_picarray_ur"></div>
+				<div class="display_picarray_ur3 display_picarray_ur clearfix"></div>
+				<div class="display_picarray_dr1"></div>
+				<div class="display_picarray_dr2"></div>
+				<div class="display_picarray_dr3"></div>
+			</div>
+		</div>
+</div>
+<div class="footer">
+	<div class="footer_content">
+		<div>
+			<span class="quickcontacts">Quick Contacts</span>
+			<span class="website">quantum.lol</span>
+			<span <div class="email">quantumx52@gmail.com</span>
+		</div>
+		<a href="#top"><span class="tothetop">TO THE TOP</span></a>
 	</div>
+</div>
 <?php get_footer()?>
